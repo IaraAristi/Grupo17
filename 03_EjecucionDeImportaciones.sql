c1f5237f03d996cf2290d862de2c9eeaab3c9b8a
agussrico <agusrico1309@gmail.com>
2025-06-21 14:00:22 -0300

    Add files via upload

--- 03_EjecucionDeImportaciones.sql
+++ 03_EjecucionDeImportaciones.sql
@@ -4,25 +4,18 @@ USE Com2900G17;
 GO
 
 EXEC ddbba.InsertarCatSocio
-    @rutaArchivo = 'C:\Users\Diego\Desktop\LOS DOMINGUEZ\Luana Unlam\Bdda\Grupo17-main\Grupo17-main\Categoria de socio.csv';
+    @rutaArchivo = 'C:\Users\agusr\Downloads\Categoria de socio.csv';
 
 SELECT * FROM ddbba.catSocio
 
 
-EXEC ddbba.InsertarCatSocio
-    @rutaArchivo = 'C:\Users\Diego\Desktop\LOS DOMINGUEZ\Luana Unlam\Bdda\Grupo17-main\Grupo17-main\Categoria de socio.csv';
-
-
-SELECT * FROM ddbba.CatSocio
-
-
 EXEC  ddbba.ImportarSociosRP
-    @rutaArchivo = 'C:\Users\Diego\Desktop\LOS DOMINGUEZ\Luana Unlam\Bdda\Grupo17-main\Grupo17-main\Responsables a cargo.csv';
+    @rutaArchivo = 'C:\Users\agusr\Downloads\Datos socios.csv';
 
 SELECT * FROM ddbba.Socio
 -------------------------
 EXEC  ddbba.ImportarSociosConGrupoFamiliar
-    @rutaArchivo = 'C:\Users\Diego\Desktop\LOS DOMINGUEZ\Luana Unlam\Bdda\Grupo17-main\Grupo17-main\Grupo familiar.csv';
+    @rutaArchivo = 'C:\Users\agusr\Downloads\Datos socios GF.csv';
 
 SELECT * FROM ddbba.Socio
 
@@ -30,24 +23,26 @@ SELECT * FROM ddbba.Socio
 -----------------
 
 EXEC  ddbba.InsertarActividades
-    @rutaArchivo = 'C:\Users\Diego\Desktop\LOS DOMINGUEZ\Luana Unlam\Bdda\Grupo17-main\Grupo17-main\Actividad deportiva.csv';
+    @rutaArchivo = 'C:\Users\agusr\Downloads\Cuota Actividad.csv';
 
 SELECT * FROM ddbba.actDeportiva
 
 -----------------------------------------
 
 EXEC ddbba.InsertarCuotasActividad
-	@rutaArchivo = 'C:\Users\Diego\Desktop\LOS DOMINGUEZ\Luana Unlam\Bdda\Grupo17-main\Grupo17-main\Actividad deportiva.csv'
+	@rutaArchivo = 'C:\Users\agusr\Downloads\Cuota Actividad.csv'
 
 SELECT * FROM ddbba.CuotaActividad
 
 -----------------------------
 
 EXEC ddbba.InsertarCuotasCatSocio
-	@rutaArchivo = 'C:\Users\Diego\Desktop\LOS DOMINGUEZ\Luana Unlam\Bdda\Grupo17-main\Grupo17-main\Categoria de socio.csv'
+	@rutaArchivo = 'C:\Users\agusr\Downloads\Cuota Cat. Socio.csv'
 
 SELECT * FROM ddbba.CuotaCatSocio
 
 ------------------------------------------
 EXEC ddbba.cargarPresentismo
-	@rutaArchivo = 'C:\Users\Diego\Desktop\LOS DOMINGUEZ\Luana Unlam\Bdda\Grupo17-main\Grupo17-main\Presentismo.csv'
+	@rutaArchivo = 'C:\Users\agusr\Downloads\Presentismo Corregido.csv'
+
+select * from ddbba.Presentismo order by fecha
