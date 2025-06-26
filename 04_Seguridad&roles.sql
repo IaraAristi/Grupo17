9f509c11e90bceef344f9b9ebb8d2e978ce9667f
Tomasl07 <104467280+Tomasl07@users.noreply.github.com>
2025-06-26 20:52:57 -0300

    Add files via upload
    Correcciones

--- 04_Seguridad&roles.sql
+++ 04_Seguridad&roles.sql
@@ -1,62 +1,62 @@
 USE master
 GO
-IF SUSER_ID(N'login_JefeTesoreria') IS NULL
+IF SUSER_ID(N'JefeTesoreria') IS NULL
 BEGIN
-    CREATE LOGIN login_Jefe_Tesoreria
+    CREATE LOGIN Jefe_Tesoreria
 		WITH PASSWORD = 'Millonario912',
 		DEFAULT_DATABASE = Com2900G17;
 END
 GO
 
-IF SUSER_ID(N'login_AdministrativoCobranza') IS NULL
+IF SUSER_ID(N'AdministrativoCobranza') IS NULL
 BEGIN
-    CREATE LOGIN login_Administrativo_Cobranza
+    CREATE LOGIN Administrativo_Cobranza
 		WITH PASSWORD = 'Megustacobrar912',
 		DEFAULT_DATABASE = Com2900G17;
 END
 GO
 
-IF SUSER_ID(N'login_AdministrativoMorosidad') IS NULL
+IF SUSER_ID(N'AdministrativoMorosidad') IS NULL
 BEGIN
-    CREATE LOGIN login_Administrativo_Morosidad
+    CREATE LOGIN Administrativo_Morosidad
         WITH PASSWORD = 'Moristeenmadrid9.12.18',
         DEFAULT_DATABASE = Com2900G17;
 END
 GO
 
-IF SUSER_ID(N'login_AdministrativoFacturacion') IS NULL
+IF SUSER_ID(N'AdministrativoFacturacion') IS NULL
 BEGIN
-    CREATE LOGIN login_Administrativo_Facturacion
+    CREATE LOGIN Administrativo_Facturacion
         WITH PASSWORD = 'Sacandodelmedio31',
         DEFAULT_DATABASE = Com2900G17;
 END
 GO
 
-IF SUSER_ID(N'login_AdministrativoSocio') IS NULL
+IF SUSER_ID(N'AdministrativoSocio') IS NULL
 BEGIN
-    CREATE LOGIN login_Administrativo_Socio
+    CREATE LOGIN Administrativo_Socio
         WITH PASSWORD = 'Labrunaeterno',
         DEFAULT_DATABASE = Com2900G17;
 END
 GO
 
-IF SUSER_ID(N'login_Socios_Web') IS NULL
+IF SUSER_ID(N'Socios_Web') IS NULL
 BEGIN
-    CREATE LOGIN login_Socios_Web
+    CREATE LOGIN Socios_Web
         WITH PASSWORD = 'Ramondiaz1996',
         DEFAULT_DATABASE = Com2900G17;
 END
 GO
 
-IF SUSER_ID(N'login_Presidente') IS NULL
+IF SUSER_ID(N'Presidente') IS NULL
 BEGIN
-    CREATE LOGIN login_Presidente
+    CREATE LOGIN Presidente
         WITH PASSWORD = 'j.milei10',
         DEFAULT_DATABASE = Com2900G17;
 END
 GO
 
-IF SUSER_ID(N'login_Vicepresidente') IS NULL
+IF SUSER_ID(N'Vicepresidente') IS NULL
 BEGIN
     CREATE LOGIN login_Vicepresidente
         WITH PASSWORD = 'vickyvillaruel10',
@@ -64,17 +64,17 @@ BEGIN
 END
 GO
 
-IF SUSER_ID(N'login_Secretario') IS NULL
+IF SUSER_ID(N'Secretario') IS NULL
 BEGIN
-    CREATE LOGIN login_Secretario
+    CREATE LOGIN Secretario
         WITH PASSWORD = 'tehizoungolunprofe',
         DEFAULT_DATABASE = Com2900G17;
 END
 GO
 
-IF SUSER_ID(N'login_Vocales') IS NULL
+IF SUSER_ID(N'Vocales') IS NULL
 BEGIN
-    CREATE LOGIN login_Vocales
+    CREATE LOGIN Vocales
         WITH PASSWORD = 'Aguanteriver9.12',
         DEFAULT_DATABASE = Com2900G17;
 END
@@ -84,33 +84,34 @@ GO
 USE Com2900G17
 GO
 
-IF DATABASE_PRINCIPAL_ID('user_Jefe_Tesoreria') IS NULL
-    CREATE USER user_Jefe_Tesoreria FOR LOGIN login_Jefe_Tesoreria WITH DEFAULT_SCHEMA = tesoreria;
+IF DATABASE_PRINCIPAL_ID('Jefe_Tesoreria') IS NULL
+    CREATE USER Jefe_Tesoreria FOR LOGIN Jefe_Tesoreria WITH DEFAULT_SCHEMA = tesoreria;
 
-IF DATABASE_PRINCIPAL_ID('user_Administrativo_Cobranza') IS NULL
-    CREATE USER user_Administrativo_Cobranza FOR LOGIN login_Administrativo_Cobranza WITH DEFAULT_SCHEMA = tesoreria;
+IF DATABASE_PRINCIPAL_ID('Administrativo_Cobranza') IS NULL
+    CREATE USER Administrativo_Cobranza FOR LOGIN Administrativo_Cobranza WITH DEFAULT_SCHEMA = tesoreria;
 
-IF DATABASE_PRINCIPAL_ID('user_Administrativo_Morosidad') IS NULL
-    CREATE USER user_Administrativo_Morosidad FOR LOGIN login_Administrativo_Morosidad WITH DEFAULT_SCHEMA = tesoreria;
+IF DATABASE_PRINCIPAL_ID('Administrativo_Morosidad') IS NULL
+    CREATE USER Administrativo_Morosidad FOR LOGIN Administrativo_Morosidad WITH DEFAULT_SCHEMA = tesoreria;
 
-IF DATABASE_PRINCIPAL_ID('user_Administrativo_Facturacion') IS NULL
-    CREATE USER user_Administrativo_Facturacion FOR LOGIN login_Administrativo_Facturacion WITH DEFAULT_SCHEMA = tesoreria;
+IF DATABASE_PRINCIPAL_ID('Administrativo_Facturacion') IS NULL
+    CREATE USER Administrativo_Facturacion FOR LOGIN Administrativo_Facturacion WITH DEFAULT_SCHEMA = tesoreria;
 	
-IF DATABASE_PRINCIPAL_ID('user_Administrativo_Socio') IS NULL
-    CREATE USER user_Administrativo_Socio FOR LOGIN login_Administrativo_Socio WITH DEFAULT_SCHEMA = socios;
+IF DATABASE_PRINCIPAL_ID('Administrativo_Socio') IS NULL
+    CREATE USER Administrativo_Socio FOR LOGIN Administrativo_Socio WITH DEFAULT_SCHEMA = socios;
 
-IF DATABASE_PRINCIPAL_ID('user_Socios_Web') IS NULL
-    CREATE USER user_Socios_Web FOR LOGIN login_Socios_Web WITH DEFAULT_SCHEMA = socios;
+IF DATABASE_PRINCIPAL_ID('Socios_Web') IS NULL
+    CREATE USER Socios_Web FOR LOGIN Socios_Web WITH DEFAULT_SCHEMA = socios;
 
-IF DATABASE_PRINCIPAL_ID('user_Presidente') IS NULL
-    CREATE USER user_Presidente FOR LOGIN login_Presidente WITH DEFAULT_SCHEMA = autoridades;
+IF DATABASE_PRINCIPAL_ID('Presidente') IS NULL
+    CREATE USER Presidente FOR LOGIN Presidente WITH DEFAULT_SCHEMA = autoridades;
 
-IF DATABASE_PRINCIPAL_ID('user_Vicepresidente') IS NULL
-    CREATE USER user_Vicepresidente FOR LOGIN login_Vicepresidente WITH DEFAULT_SCHEMA = autoridades;
+IF DATABASE_PRINCIPAL_ID('Vicepresidente') IS NULL
+    CREATE USER Vicepresidente FOR LOGIN Vicepresidente WITH DEFAULT_SCHEMA = autoridades;
 
-IF DATABASE_PRINCIPAL_ID('user_Secretario') IS NULL
-    CREATE USER user_Secretario FOR LOGIN login_Secretario WITH DEFAULT_SCHEMA = autoridades;
+IF DATABASE_PRINCIPAL_ID('Secretario') IS NULL
+    CREATE USER Secretario FOR LOGIN Secretario WITH DEFAULT_SCHEMA = autoridades;
 
 IF DATABASE_PRINCIPAL_ID('user_Vocales') IS NULL
-    CREATE USER user_Vocales FOR LOGIN login_Vocales WITH DEFAULT_SCHEMA = autoridades;
+    CREATE USER Vocales FOR LOGIN Vocales WITH DEFAULT_SCHEMA = autoridades;
 GO
+
